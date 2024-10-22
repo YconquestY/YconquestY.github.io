e6cb7fb774869d67d0c393ea733a1cf27875986b
Will Yu <yconquesty@gmail.com>
2024-10-22 17:36:10 +0200

    customize font text: poppins; code: cascadia code; Chinese characters: 霞鹜文楷

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -12,4 +12,20 @@ export default defineUserConfig({
     theme,
     // enable PWA
     //shouldPrefetch: false,
+
+    head: [
+        ["link", {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com"
+        }],
+        ["link", {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossorigin: ""
+        }],
+        ["link", {
+            rel: "stylesheet",
+            href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+        }]
+    ]
 });
